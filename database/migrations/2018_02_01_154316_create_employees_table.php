fef5533a32b04b419ec771dcb535e9941b420bd3
Trần Minh Đức <haiyenho2201@gmail.com>
2018-02-06 14:59:18 +0700

    Auto stash before revert of "."

---
+++ database/migrations/2018_02_01_154316_create_employees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateEmployeesTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('name');
+            $table->text('phone_number');
+            $table->text('address');
+            $table->datetime('date');
+            $table->text('salary');
+            $table->text('day_work')->nullable();
+            $table->text('over_time')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('employees');
+    }
+}
