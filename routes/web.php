4ee06f29469f316d3d696a544a61ee0e1e5b2032
Trần Minh Đức <ductranminhitqb@gmail.com>
2018-03-12 00:44:41 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ Route::group(['middleware' => 'editor'], function () {
 Route::group(['middleware' => 'auth'], function () {
     Route::any('/profile', 'HomeController@profile');
     Route::any('/change_pass', 'HomeController@change');
+    Route::get('/dashboard', 'DashboardController@index');
+    Route::get('/', 'DashboardController@index');
 });
-Route::get('/home', 'HomeController@index');
-Route::get('/', 'HomeController@index');
 Route::resource('machines', 'MachineController');
 Route::any('data_json', 'MachineController@getDataForDataTable');
 
